e793e568c6f9d60df3dbacabcd64ad722d60cfc0
WysperOtaku <wysperotaku@gmail.com>
2025-06-03 15:06:26 +0200

    Ping improvement and fixing

--- commands/utilities/ping.js
+++ commands/utilities/ping.js
@@ -1,7 +1,16 @@
 module.exports ={
   name: 'ping',
   description: 'Responde pong',
-  execute(client) {
-    message.channel.send('🏓 Pong!');
+  async execute(message) {
+    // Tiempo antes de enviar mensaje
+    const sent = await message.channel.send('🏓 Calculando ping...');
+
+    const botLatency = sent.createdTimestamp - message.createdTimestamp;
+    const apiLatency = Math.round(message.client.ws.ping);
+
+    await sent.edit(`🏓 Pong!
+
+**⏱ Latencia del bot:** \`${botLatency}ms\`
+**📡 Latencia de la API:** \`${apiLatency}ms\``);
   }
 };
