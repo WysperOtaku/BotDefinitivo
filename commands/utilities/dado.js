254a00e713a7f7960e9c2dc12741b19c16cedc67
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-06-04 18:59:58 +0200

    chists and dado features create

---
+++ commands/utilities/dado.js
@@ -0,0 +1,20 @@
+const { execute } = require("./chists");
+
+module.exports = {
+    name: "dado",
+    execute: async(msg) => {
+        if (msg.author.bot) return; // Ignora mensajes de bots
+
+        const args = msg.content.trim().split(/\s+/)
+            
+        let maxNum = parseInt(args[1]);
+
+        if (maxNum < 2) return msg.channel.send("El numero no puede ser menor a 2 si no no tiene sentido 😡.")
+        else if (isNaN(maxNum)){
+            msg.channel.send("Como no has introducido numero lo hare con 6 🎲.");
+            maxNum = 6;
+        }
+
+        msg.reply("Has sacado un... " + Math.floor(Math.random() * maxNum + 1) + " 🎲");
+    }
+};
